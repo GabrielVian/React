c81047b734dcdb7ebe97626fab4f1c47ad12b009
Gabriel Vian <66270578+GabrielVian@users.noreply.github.com>
2026-08-19 22:04:14 -0300

    Checkout page

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import HomePage from './pages/HomePage'
 import { Routes, Route } from 'react-router';
+import CheckoutPage from './pages/CheckoutPage';
 
 function App() {
 
   return (
     <Routes>
       <Route index element={<HomePage/>}/>
-      <Route path='checkout' element={<>TEST</>}/>
+      <Route path='checkout' element={<CheckoutPage/>}/>
     </Routes>
   )
 }
