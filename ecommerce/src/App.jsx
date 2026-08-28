7bbcfb23cb48138d0330c52da782a0a6fc4db762
Gabriel Vian <66270578+GabrielVian@users.noreply.github.com>
2026-08-28 16:14:29 -0300

    .

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -2,13 +2,17 @@ import './App.css'
 import HomePage from './pages/HomePage'
 import { Routes, Route } from 'react-router';
 import CheckoutPage from './pages/CheckoutPage';
+import OrdersPage from './pages/OrdersPage';
+import TrackingPage from './pages/TrackingPage';
 
 function App() {
 
   return (
     <Routes>
       <Route index element={<HomePage/>}/>
+      <Route path='orders' element={<OrdersPage/>}/>
       <Route path='checkout' element={<CheckoutPage/>}/>
+      <Route path='tracking' element={<TrackingPage/>}/>
     </Routes>
   )
 }
