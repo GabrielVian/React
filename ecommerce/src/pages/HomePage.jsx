490c6a508f336f61f2d1a811c327c1bde56533fe
Gabriel Vian <66270578+GabrielVian@users.noreply.github.com>
2026-08-29 13:35:04 -0300

    Cart from backend and display qtd

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -7,11 +7,17 @@ import axios from 'axios';
 function HomePage() {
     // const products = [];
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
 
     useEffect(() => {
         axios.get('http://localhost:3000/api/products')
             .then((response) => {
                 setProducts(response.data)
+            })
+        axios.get('http://localhost:3000/api/cart-items')
+            .then((response)=>{
+                setCart(response.data)
             })
     }, [])
 
@@ -20,7 +26,7 @@ function HomePage() {
         <>
             <title>Ecommerce Project</title>
             <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
-            <Header />
+            <Header cart={cart}/>
 
 
             <div className="home-page">
