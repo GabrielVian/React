4414070a604d4aa61370187b28d3d51df57521f1
Gabriel Vian <66270578+GabrielVian@users.noreply.github.com>
2026-08-29 12:21:12 -0300

    Lesson 6k

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router';
 import CheckoutPage from './pages/checkout/CheckoutPage';
 import OrdersPage from './pages/OrdersPage';
 import TrackingPage from './pages/TrackingPage';
+import { NotFound } from './pages/NotFound';
 // Lesson 6h already done
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Route path='orders' element={<OrdersPage/>}/>
       <Route path='checkout' element={<CheckoutPage/>}/>
       <Route path='tracking' element={<TrackingPage/>}/>
+      <Route path='*' element={<NotFound/>}/>
     </Routes>
   )
 }
---
+++ ecommerce/src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import { Header } from "../components/Header";
+
+export function NotFound(){
+    return (
+        <>
+            <Header/>
+            <br />
+            <br />
+            <br />
+            <h1>
+                PAGE NOT FOUND
+            </h1>
+        </>
+    );
+}
