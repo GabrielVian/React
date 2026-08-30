6a5ff8fa4a6de366f14853ba30817d52d6b0e9ad
Gabriel Vian <66270578+GabrielVian@users.noreply.github.com>
2026-08-30 14:37:34 -0300

    Lesson 7h

--- ecommerce/src/pages/TrackingPage.jsx
+++ ecommerce/src/pages/TrackingPage.jsx
@@ -1,14 +1,14 @@
 import './TrackingPage.css';
 import {Header} from '../components/Header'; 
 import { Link } from 'react-router';
-
+// Already done it 
 function TrackingPage({ cart }) {
     return (
         <>
             <title>Tracking</title>
             <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
 
-            <Header cart={cart} />
+            <Header cart={cart} /> 
 
             <div className="tracking-page">
                 <div className="order-tracking">
