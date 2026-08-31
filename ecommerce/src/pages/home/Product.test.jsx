e797d06e4110d65304ff7eebbe41a6302630c619
Gabriel Vian <66270578+GabrielVian@users.noreply.github.com>
2026-08-31 18:46:34 -0300

    Before each in testing

--- ecommerce/src/pages/home/Product.test.jsx
+++ ecommerce/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import {it, expect, describe,vi } from 'vitest';
+import {it, expect, describe,vi, beforeEach } from 'vitest';
 import {render, screen} from '@testing-library/react'
 import Product from './Product'
 import userEvent from '@testing-library/user-event';
@@ -6,8 +6,11 @@ import axios from 'axios';
 vi.mock('axios')
 
 describe('Product component', ()=>{
-    it('displays the productt details correctly', () => {
-        const product = {
+    let product = {};
+    let loadCart;
+
+    beforeEach(() => {
+        product={
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -17,9 +20,14 @@ describe('Product component', ()=>{
             },
             priceCents: 1090,
             keywords: ["socks", "sports", "apparel"]
-        }
+        },
+        loadCart = vi.fn();
+    })
 
-        const loadCart = vi.fn();
+    it('displays the productt details correctly', () => {
+        
+
+        
 
         render(<Product p={product} loadCart={loadCart}/>)
 
@@ -44,19 +52,6 @@ describe('Product component', ()=>{
         ).toBeInTheDocument();
     })
     it("it adds products to cart", async ()=>{
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        }
-
-        const loadCart = vi.fn();
 
         render(<Product p={product} loadCart={loadCart}/>)
 
